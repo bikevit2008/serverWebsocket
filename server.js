97eadd4ae5de403163ffa6cf169da124078a5061
Vitaly <bikevit2008@rambler.ru>
2017-08-20 20:37:15 +0300

    Use ws handler

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const helmet = require('helmet')
 const expressLogging = require('express-logging')
 const logger = require('logops')
 
+const wsHandle = require('./websocket')
+
 const port = 80
 
 app.use(helmet())
@@ -13,47 +15,8 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.ws('/', (ws, req) => {
-      console.log('Client connected')
-      
-      ws.on('message', msg => {
-        console.log('Meassage got: ' + msg)
-        try{
-            let jsonMsg = JSON.parse(msg)
-            let method = jsonMsg.method
-            if(method){
-                switch(method){
-                    case 'hi':
-                        ws.send('HI from server')
-                        break;
-                    default:
-                        let error = new Error("Method unknown", 501) //Тут метод и правда неизвестен
-                        let jsonError = JSON.stringify(error)
-                        ws.send(jsonError)  
-                }
-            }
-            else if (!jsonMsg.method) {
-                let error = new Error("Method unknown", 501) //Тут отсутствует метод 
-                let jsonError = JSON.stringify(error)
-                ws.send(jsonError)
-            }
-            else{
-                let error = new Error("Internal Server Error", 500)
-                let jsonError = JSON.stringify(error)
-                ws.send(jsonError)
-            }
-        }
-        catch (errorParse){
-            let error = new Error("Method unknown", 501) //Надо поменять на неферный вормат данных или что-то вроде
-            let jsonError = JSON.stringify(error)
-            ws.send(jsonError)
-            console.log('Unknow user testing our service. Thats bad.')
-            console.log(msg)
-            console.log(errorParse.name)
-            console.log(errorParse.message)
-        }
-      })
-      
+app.ws('/', function(ws, req){
+    wsHandle.handle(ws, req)
 })
 
 class Error{
